23b279928481e493031e9b77f403fb46a3a99257
Ravi Gupta <ravik.rkg@gmail.com>
2017-05-05 18:23:59 +0530

    Add First Screen

--- app/src/main/java/com/phoebus_ravi/www/chiku/MainActivity.java
+++ app/src/main/java/com/phoebus_ravi/www/chiku/MainActivity.java
@@ -2,6 +2,8 @@ package com.phoebus_ravi.www.chiku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickedButterfly(View view) {
+
+
+        Toast.makeText(this,"Thank you !!!",Toast.LENGTH_SHORT).show();
+        finish();
     }
 }
